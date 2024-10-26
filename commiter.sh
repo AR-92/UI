2b06e1f7ba2da616b1bdde5818004ecf21524177
AR-92 <muhammadahmedrana1992@yahoo.com>
2024-10-27 03:11:13 +0500

    Changes committed and pushed at Sun Oct 27 03:11:13 PKT 2024, Auto commit from commiter script !

--- commiter.sh
+++ commiter.sh
@@ -7,10 +7,13 @@ while true; do
     # Check if there are any changes
     if test -n "$(git status --porcelain)" ; then
 
+        echo -e "\033[36m ⮞ ҒORMΔΓIṈG ĦΓML IṈ ΓĦE BΔCKGROUṈD ᕈLEΔSE WΔIΓ ! \033[0m"
+
         # find . -type f -name "*.html" -exec perl -i -pe 's/(<[^>]+>)(\n)?/\1\n\n/g' {} \;
         prettier --write "**/*.html" --single-attribute-per-line --html-whitespace-sensitivity ignore --bracket-same-line > /dev/null 2>&1
         # prettier --write "**/*.html" --tab-width 4
         # Add changes to the staging area
+        echo -e "\033[31m ᙅᕼᗩᑎᘜᙓS ᗩᖇᙓ ᖴOᙀᑎᗪ ᙅOᙏᙏIᑎT IT TO ᘜITᕼᙀᙖ !  \033[0m"
         git add .
 
         # Commit the changes
